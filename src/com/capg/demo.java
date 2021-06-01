99441d65ef2a71a34b30dcde5b4e00defbe9a631
VenkyVikky <VenkyVikky@LAPTOP-FPGMJP8U>
2021-06-01 19:18:37 +0530

    DEMO PROJECT ADDED

---
+++ src/com/capg/demo.java
@@ -0,0 +1,13 @@
+package com.capg;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+		System.out.println("Venky");
+
+	}
+
+}
